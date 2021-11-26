284cb821e96273c479b083b13f541d6c9339593b
danielandricic <dani.andricic@gmail.com>
2021-11-26 13:17:57 +0100

    greeting event working async

--- vertx/src/main/java/at/htl/service/GreetingService.java
+++ vertx/src/main/java/at/htl/service/GreetingService.java
@@ -1,14 +1,20 @@
 package at.htl.service;
 
 import io.quarkus.vertx.ConsumeEvent;
+import io.smallrye.mutiny.Uni;
+import org.eclipse.microprofile.context.ManagedExecutor;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 @ApplicationScoped
 public class GreetingService {
 
-    @ConsumeEvent
-    public String consume(String name) {
-        return name.toUpperCase();
+    @Inject
+    ManagedExecutor executor;
+
+    @ConsumeEvent("greeting")
+    public Uni<String> consume(String name) {
+        return Uni.createFrom().item(() -> name.toUpperCase()).emitOn(executor);
     }
 }
