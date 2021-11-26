e7c9126e19b592c70cff0ee243f0b8a6e1bf9c05
danielandricic <dani.andricic@gmail.com>
2021-11-26 13:12:07 +0100

    add GreetingService

---
+++ vertx/src/main/java/at/htl/service/GreetingService.java
@@ -0,0 +1,14 @@
+package at.htl.service;
+
+import io.quarkus.vertx.ConsumeEvent;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+    @ConsumeEvent
+    public String consume(String name) {
+        return name.toUpperCase();
+    }
+}
